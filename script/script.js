79950014520ef8085e4d425eabc24726d71691fd
Ayeong Kim <fay3349@naver.com>
2022-12-12 18:18:06 +0900

    third commit

--- script/script.js
+++ script/script.js
@@ -13,9 +13,9 @@ if (myWindow > 1104) {
 
     
 
-    controller.addTween(10, TweenMax.from($('.bean_img_box'), 2.5, {css:{left:'-500px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.from($('.bean_img_box'), 2.5, {css:{left:'-400px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
     controller.addTween(10, TweenMax.to($('.bean_img_box'), 2.5, {css:{left:'10%', opacity:1}, ease: Quad.easeOut}), 1, 1000);
-    controller.addTween(10, TweenMax.from($('.bean_txt_box'), 2.5, {css:{right:'-500px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.from($('.bean_txt_box'), 2.5, {css:{right:'-400px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
     controller.addTween(10, TweenMax.to($('.bean_txt_box'), 2.5, {css:{right:'10%', opacity:1}, ease: Quad.easeOut}), 1, 1000);
 
 } else if ((myWindow > 960)) {
@@ -40,4 +40,34 @@ controller.addTween(10, TweenMax.to($('.reserve_visual'), 2.5, {css:{opacity:1},
 
 
 
+//favWrap
+var controller = $.superscrollorama({
+    triggerAtCenter: true
 });
+
+if (myWindow > 1104) {
+
+    var controller = $.superscrollorama();
+
+    
+
+    controller.addTween(10, TweenMax.from($('.winter_title'), 2.5, {css:{left:'-800px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.to($('.winter_title'), 2.5, {css:{left:'10%', opacity:1}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.from($('.winter_txt_box'), 2.5, {css:{left:'-800px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.to($('.winter_txt_box'), 2.5, {css:{left:'0%', opacity:1}, ease: Quad.easeOut}), 1, 1000);
+
+} else if ((myWindow > 960)) {
+
+    var controller = $.superscrollorama();
+
+    controller.addTween(10, TweenMax.from($('.winter_title'), 2.5, {css:{left:'-50%', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.to($('.winter_title'), 2.5, {css:{left:'0', opacity:1}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.from($('.winter_txt_box'), 2.5, {css:{left:'-50%', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.to($('.winter_txt_box'), 2.5, {css:{left:'0', opacity:1}, ease: Quad.easeOut}), 1, 1000);
+};
+
+
+
+
+});
+
