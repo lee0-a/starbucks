e6198ac453b17161ae57917bebbe9f55ba57c5f7
Ayeong Kim <fay3349@naver.com>
2022-12-14 16:31:59 +0900

    fifth commit

--- script/script.js
+++ script/script.js
@@ -13,9 +13,9 @@ if (myWindow > 1104) {
 
     
 
-    controller.addTween(10, TweenMax.from($('.bean_img_box'), 2.5, {css:{left:'-400px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.from($('.bean_img_box'), 2.5, {css:{left:'-300px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
     controller.addTween(10, TweenMax.to($('.bean_img_box'), 2.5, {css:{left:'10%', opacity:1}, ease: Quad.easeOut}), 1, 1000);
-    controller.addTween(10, TweenMax.from($('.bean_txt_box'), 2.5, {css:{right:'-400px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
+    controller.addTween(10, TweenMax.from($('.bean_txt_box'), 2.5, {css:{right:'-300px', opacity:0}, ease: Quad.easeOut}), 1, 1000);
     controller.addTween(10, TweenMax.to($('.bean_txt_box'), 2.5, {css:{right:'10%', opacity:1}, ease: Quad.easeOut}), 1, 1000);
 
 } else if ((myWindow > 960)) {
@@ -68,8 +68,8 @@ if (myWindow > 1104) {
 
 
 
-$('header > div > nav > ul > li > h2 > a').bind('mouseover focus', function (e) {
-    $('header > div > nav > ul > li > h2 > a').removeClass('on');
+$('header .header_wrap > div > nav > ul > li > h2 > a').bind('mouseover focus', function (e) {
+    $('header .header_wrap > div > nav > ul > li > h2 > a').removeClass('on');
     $('.sub_wrap').hide();
 
     $(this).addClass('on');
@@ -79,10 +79,11 @@ $('header > div > nav > ul > li > h2 > a').bind('mouseover focus', function (e) 
   });
 
   $('.sub_inner').bind('mouseleave', function () {
-    $('header > div > nav > ul > li > h2 > a').removeClass('on');
+    $('header .header_wrap > div > nav > ul > li > h2 > a').removeClass('on');
     $('.sub_wrap').slideUp();
   });
 
  
+  alert("본 사이트는 상업적 목적이 아닌 개인 포트폴리오의 용도로 만들어졌습니다.스타벅스 사이트 클론코딩한 작업물이며, 모든 저작권은 스타벅스에 있습니다.");
 });
 
